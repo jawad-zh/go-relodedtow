90e67f5e91c1ab13931f2c91af8148559c0e5905
jazahraoui <zahraoui.jawad8@gmail.com>
2025-07-04 15:43:26 +0100

    push

--- functionss/splitPunc.go
+++ functionss/splitPunc.go
@@ -3,21 +3,23 @@ package goreloaded
 func SplitPunc(s string) []string {
 	var word string
 	var words []string
-	for i := 0; i < len(s); i++ {
-		if s[i] != ' ' && !Only(string(s[i])) {
-			word += string(s[i])
+	runes := []rune(s)
+	for i := 0; i < len(runes); i++ {
+		char := runes[i]
+		if char != ' ' && !Only(string(char)) {
+			word += string(char)
 		} else {
 			if word != "" {
 				words = append(words, word)
 				word = ""
 			}
-			if Only(string(s[i])) {
-				words = append(words, string(s[i]))
+			if Only(string(char)) {
+				words = append(words, string(char))
 			}
 		}
 	}
-	if word != ""{
-		words = append(words,word)
+	if word != "" {
+		words = append(words, word)
 	}
 	return words
 }
