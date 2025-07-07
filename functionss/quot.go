0db9bb167441b2c6967618d3a72b842353696224
jawad-zh <zahraoui.jawad20@gmail.com>
2025-07-07 17:58:38 +0100

    push

--- functionss/quot.go
+++ functionss/quot.go
@@ -47,48 +47,45 @@ import (
 // }
 
 
-
 func Quote(reloaded []string) []string {
-	s := strings.Join(reloaded, " ")
-	var open bool
-	index := 0
-	var str string
-
-	for i := 0; i < len(s); i++ {
-		c := s[i]
-
-		var prev, next byte
-		if i > 0 {
-			prev = s[i-1]
-		}
-		if i < len(s)-1 {
-			next = s[i+1]
-		}
-
-		if i != len(s)-1 {
-			if !open {
-				if c == '\'' && (i > 0 && (next == ' ' || prev == ' ' || next == '\'' || prev == '\'')) {
-					str += s[index:i]
-					open = true
-					index = i
-				}
-			} else if open && c == '\'' && (i > 0 && (next == ' ' || prev == ' ' || next == '\'' || prev == '\'')) {
-				str += " '" + strings.TrimSpace(s[index+1:i]) + "' "
-				open = false
-				index = i + 1
-				continue
-			}
-		}
-
-		if i == len(s)-1 {
-			if open && c == '\'' {
-				str += "'" + strings.TrimSpace(s[index+1:]) + "'"
-			} else {
-				str += s[index:]
-			}
-		}
-	}
-
-	return strings.Fields(str)
+    s := strings.Join(reloaded, " ")
+    var open bool
+    index := 0
+    var str string
+    for i, c := range s {
+        if i != len(s)-1 && i != 0 {
+            if !open {
+                if c == '\'' && (s[i+1] == ' ' || s[i-1] == ' ' || s[i+1] == '\'' || s[i-1] == '\'' || Only(string(s[i+1])) || Only(string(s[i-1]))) {
+                    str += s[index:i]
+                    open = true
+                    index = i
+                }
+            } else if open && c == '\'' && (s[i+1] == ' ' || s[i-1] == ' ' || s[i+1] == '\'' || s[i-1] == '\'' || Only(string(s[i+1])) || Only(string(s[i-1]))){
+                if Only(string(s[i+1])) || Only(string(s[i-1])) {
+                    str += " " + "'" + strings.TrimSpace(s[index+1:i]) + "'"
+                    open = false
+                    index = i + 1
+                    continue
+                }
+                str += " " + "'" + strings.TrimSpace(s[index+1:i]) + "'" + " "
+                open = false
+                index = i + 1
+                continue
+            }
+        } else if i == 0 {
+            if c == '\'' {
+                str += s[index:i]
+                open = true
+                index = i
+            }
+        }
+        if i == len(s)-1 {
+            if open && c == '\'' {
+                str += " " + "'" + strings.TrimSpace(s[index+1:len(s)-1]) + "'"
+            } else {
+                str += s[index:]
+            }
+        }
+    }
+    return strings.Fields(str)
 }
-
